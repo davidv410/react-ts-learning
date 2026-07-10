b60a5dbb61fdcd83bc59606b899516b7ff249ede
davidv410 <davidvrgoc99@gmail.com>
2026-07-10 17:49:30 +0200

    interceptor tweaks and other small changes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -16,7 +16,7 @@ api.interceptors.response.use((response) => response,
             return Promise.reject(error)
         }
 
-        if(error.response?.status === 401 && !originalRequest._retry){
+        if(error.response?.status === 401 && !originalRequest._retry && !originalRequest.url?.includes('/auth/me')){
             originalRequest._retry = true
 
             try{
--- src/pages/showtimeDetail.tsx
+++ src/pages/showtimeDetail.tsx
@@ -1,37 +1,41 @@
-import {useShowtime} from "@/features/showtimes/hooks/useShowtime.ts";
-import {useParams} from "react-router-dom";
-import {Seats} from "@/pages/seats.tsx";
-import {EditShowtimeForm} from "@/features/showtimes/components/EditShowtimeForm.tsx";
+import { useShowtime } from "@/features/showtimes/hooks/useShowtime.ts";
+import { useParams } from "react-router-dom";
+import { Seats } from "@/pages/seats.tsx";
+import { EditShowtimeForm } from "@/features/showtimes/components/EditShowtimeForm.tsx";
+import { useAuth } from "@/features/auth/context.tsx";
 
 export const ShowtimeDetail = () => {
     const { id = '' } = useParams()
 
+    const { user } = useAuth()
+
     const { data, isLoading, error } = useShowtime(id)
 
-    if(isLoading) return <p>Loading...</p>
-    if(error) return <p>{error.message}</p>
-    if(!id) return <p>Movie not found</p>
+    if (isLoading) return <p>Loading...</p>
+    if (error) return <p>{error.message}</p>
+    if (!id) return <p>Movie not found</p>
 
-    return(
+    return (
         <>
             {data && (
-            <div>
-                <p>{data.id}</p>
-            </div>
+                <div>
+                    <p>{data.id}</p>
+                </div>
             )}
 
             <section>
                 <Seats showtimeId={id}/>
             </section>
 
-
-            <div>
-                {data ?
-                    <EditShowtimeForm showtimeData={data} />
-                :
-                    <div>Showtime not found.</div>
-                }
-            </div>
+            {user?.role === 'admin' &&
+                <div>
+                    {data ?
+                        <EditShowtimeForm showtimeData={data}/>
+                        :
+                        <div>Showtime not found.</div>
+                    }
+                </div>
+            }
         </>
     )
 }
